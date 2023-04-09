ad454a66d8742a512058c0526cd8ef7230b8e81c
James Hodgkinson <james@terminaloutcomes.com>
2023-04-09 16:36:06 +1000

    fixing up testing

---
+++ tests/test_click.py
@@ -0,0 +1,9 @@
+from click.testing import CliRunner
+
+from certbrother import cli
+
+
+def test_hello_world() -> None:
+  runner = CliRunner()
+  result = runner.invoke(cli, ['--help'])
+  assert result.exit_code == 0
