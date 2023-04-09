abeeb51e4cdce204982f4d5484b45eaa8b1e8512
James Hodgkinson <james@terminaloutcomes.com>
2023-04-09 21:04:54 +1000

    cleaning up tests

--- tests/test_click.py
+++ tests/test_click.py
@@ -1,9 +1,15 @@
+import os
+from pathlib import Path
+
 from click.testing import CliRunner
 
 from certbrother import cli
 
 
 def test_hello_world() -> None:
+
+  if os.getenv("CI"): # running in github actions
+    Path('.env').write_text(Path('.env.example').read_text())
   runner = CliRunner()
   result = runner.invoke(cli, ['--help'])
   assert result.exit_code == 0
